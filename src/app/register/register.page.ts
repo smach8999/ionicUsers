20144ce1850416d5f9a5e7f1f45931d736bf97e4
Jason Snider <root@jasonsnider.com>
2019-11-25 17:38:14 -0600

    Wired the registration form up to the API

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService } from '../users.service';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 import { User } from '../user.model';
+
 
 @Component({
   selector: 'app-register',
@@ -12,9 +15,46 @@ export class RegisterPage implements OnInit {
   user:User = new User();
   errors: any = {};
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  response(response): void{
+    
+    if(response.success===false){
+      
+      if( response.errors.name == 'MissingUsernameError' ){
+        this.errors.username = 'Please enter a username';
+      }
+
+      if( response.errors.name == 'UserExistsError' ){
+        this.errors.username = 'A user with the given username is already registered';
+      }
+
+      if( response.errors.name == 'MissingPasswordError' ){
+        this.errors.password = 'Please enter a password';
+      }
+
+      if( response.errors.email ){
+        this.errors.email = response.errors.errors.email.message;
+      }
+
+    }
+
+    if(response.success===true){
+      this.router.navigate(['/login']);
+    }
+  }
+
+  onSubmit(): void{
+    this.authService.register(this.user).subscribe(
+      (response:any) => {
+        this.response(response);
+      }
+    );
   }
 
 }
