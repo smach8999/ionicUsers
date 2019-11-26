be8c2bff78e75ab40a59dfc46c3db4d7f22fbafb
Jason Snider <root@jasonsnider.com>
2019-11-25 18:39:16 -0600

    Add a getUser() wrapper

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { UsersService } from '../users.service';
+import { User } from '../user.model'; 
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  user: User;
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+      console.log(params['userId']);
+      this.getUser(params['userId']);
+    });
   }
 
+  getUser(id:string):void {
+    this.usersService.getUser(id).subscribe(
+      (response:any)=>{
+        console.log(response);
+        this.user = response.user;
+      }
+    );
+  }
 }
